a9b0c45eed265b038dfce9d719078fa6a72d8f37
Chad Clarke <chadclarke@bell.net>
2016-11-03 20:51:49 -0400

    First Commit - Added all Lab3 files

---
+++ student-details.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// reference the model binding library
+using System.Web.ModelBinding;
+
+namespace Week6
+{
+    public partial class student_details : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false) {
+                //check the url and see if there is an id to know if we are adding or editiing the data 
+                if (!String.IsNullOrEmpty(Request.QueryString["StudentID"]))
+                {
+                    //get the id from the url of the object dont forget to convert string to number
+                    Int32 StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+
+                    //connect to the db 
+                    var conn = new ContosoEntities();
+
+                    // find the selected departemnt 
+                    var stud = (from d in conn.Students
+                                where d.StudentID == StudentID
+                                select d).FirstOrDefault();
+
+                    //populate the form with the users data 
+                    txtLastName.Text = stud.LastName;
+                    txtFirstMidName.Text = stud.FirstMidName;
+                    txtEnrollmentDate.Text = stud.EnrollmentDate.ToString();
+                }
+            }
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+            //check if we have an id to decide if we are adding or editing 
+            Int32 StudentID = 0;
+
+            if (!String.IsNullOrEmpty(Request.QueryString["StudentID"]))
+            {
+                StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+            }
+
+            // connect
+            var conn = new ContosoEntities();
+
+            // use the Student class to create a new Student object
+            Student s = new Student();
+
+            // fill the properties of the new student object
+            s.LastName = txtLastName.Text;
+            s.FirstMidName = txtFirstMidName.Text;
+            s.EnrollmentDate  = Convert.ToDateTime(txtEnrollmentDate.Text);
+
+            // save the new object to the database
+            if (StudentID == 0)
+            {
+                conn.Students.Add(s);
+                conn.SaveChanges();
+            }
+            else
+            {
+                s.StudentID = StudentID;
+                conn.Students.Attach(s);
+                conn.Entry(s).State = System.Data.Entity.EntityState.Modified;
+            }
+            conn.SaveChanges();
+            // redirect to the students page
+            Response.Redirect("students.aspx");
+        }
+    }
+}
---
+++ students.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// add reference to access the database
+using System.Web.ModelBinding;
+
+namespace Week6
+{
+    public partial class students : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            // get the departments and display in the gridview
+            getStudents();
+        }
+
+        protected void getStudents()
+        {
+            // connect to db
+            var conn = new ContosoEntities();
+
+            // run the query using LINQ
+            var Students = from s in conn.Students
+                           select s;
+
+            // display query result in gridview
+            grdStudents.DataSource = Students.ToList();
+            grdStudents.DataBind();
+
+        }
+
+        protected void grdStudents_RowDeleting(object sender, GridViewDeletedEventArgs e)
+        {
+
+         //// This part is not functioning and i was unble to find decent documentation online to solve 
+            // 1. determine which row in the grid the user clicked
+            Int32 gridIndex = e.RowIndex;
+
+            // 2. find the DepartmentID value in the selected row
+            Int32 StudentID = Convert.ToInt32(grdStudents.DataKeys[gridIndex].Value);
+
+            // 3. connect to the db
+            var conn = new ContosoEntities();
+
+            Student s = new Student();
+            s.StudentID = StudentID;
+            conn.Students.Attach(s);
+            conn.Students.Remove(s);
+            conn.SaveChanges();
+
+            // 5. refresh the table
+            getStudents();
+
+
+        }
+
+    }
+}
